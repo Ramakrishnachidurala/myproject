d21de23f0b54312858b80c563da0f18e7d2dc33c
Ramakrishnachidurala <rk.chidurala20dec@gmail.com>
2022-05-11 17:54:03 +0530

    Ready for deployment to heroku.

---
+++ personal_portfolio/wsgi.py
@@ -0,0 +1,12 @@
+"""
+WSGI config for personal_portfolio project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
